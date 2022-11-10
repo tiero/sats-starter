91bf8eff340bbeadef05c457418757465a087124
tiero <3596602+tiero@users.noreply.github.com>
2022-11-10 20:28:12 +0100

    gh-action: ci

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
-  basePath: '/sats-starter',
+  //basePath: '/sats-starter',
   reactStrictMode: false,
   swcMinify: true,
   images: {
